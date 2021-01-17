918921a85247688748b50e3ab8b6976b28139bbf
mc <snoopy02m@yahoo.com.tw>
2021-01-17 15:27:52 +0800

    add line code

--- application.py
+++ application.py
@@ -25,6 +25,25 @@ from PIL import Image, ImageDraw, ImageFont
 import time
 
 app = Flask(__name__)
+LINE_SECRET = "d2abd6336bb6578a321bcb9e9070cd4f"]
+LINE_TOKEN = "kE9Z0tufekB1YcCXp2y/UbwhO7O5Nw+bKrEpuxHVWXuS03iHFlccd0YFQnMtLMsOBibZCj0nozVc9P1W6BZRNT0r22ZkgocgLfrotK3T9CadyKPeVApvWh/yzm6NWBYQf4u22rG4KhU1Ph8VDQt2uQdB04t89/1O/w1cDnyilFU="
+LINE_BOT = LineBotApi(LINE_TOKEN)
+HANDLER = WebhookHandler(LINE_SECRET)
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    # X-Line-Signature: 數位簽章
+    signature = request.headers["X-Line-Signature"]
+    print(signature)
+    body = request.get_data(as_text=True)
+    print(body)
+    try:
+        HANDLER.handle(body, signature)
+    except InvalidSignatureError:
+        print("Check the channel secret/access token.")
+        abort(400)
+    return "OK"
+
 
 @app.route("/")
 def hello():
